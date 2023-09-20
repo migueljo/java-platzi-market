11982742b5ddf0e2b68d93f50f937ac36d1a04a0
Miguel <migueljomartinez@gmail.com>
2023-09-19 20:59:44 -0500

    First commit

---
+++ src/main/java/com/miguel/platzimarket/JavaPlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPlatziMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/platzimarket/JavaPlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.platzimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
