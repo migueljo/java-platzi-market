656d0a8945ebf3e77bd28a37092c92788ec922a5
Miguel <migueljomartinez@gmail.com>
2023-09-21 20:21:26 -0500

    First endpoint with spring boot

--- src/main/java/com/miguel/platzimarket/HolaMundoController.java
+++ src/main/java/com/miguel/platzimarket/HolaMundoController.java
@@ -1,2 +1,14 @@
-package com.miguel.platzimarket;public class HolaMundoController {
+package com.miguel.platzimarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+	@GetMapping("/hola")
+	public String saludar() {
+		return "Nunca pares de aprender! 🚀";
+	}
 }
